0deff63c1a9f4c3bbd137160344a961fcf62ad58
chrmorfeus <chrmorfeus@live.com>
2023-08-07 17:43:03 -0600

    Actualizacion script.js y contact-us.html

---
+++ script.js
@@ -0,0 +1,6 @@
+// Contact us script section
+function getDataForm() {
+    let fname = document.getElementById("fname").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+}
